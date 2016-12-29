7d42cf724259f5d778d84220bc24b8b7ba02b36a
msourav2795 <msourav2795@gmail.com>
2016-12-29 23:30:13 +0530

    add

---
+++ js/hover.js
@@ -0,0 +1,16 @@
+$(".circle-1").bind({
+'mouseenter': function(){$("#page1").css('background-image', 'url(images/back1.jpg)');}, 
+'mouseleave': function(){$("#page1").css('background-image', 'url(images/light.jpg)');}
+});
+$(".circle-2").bind({
+'mouseenter': function(){$("#page1").css('background-image', 'url(images/back2.jpg)');}, 
+'mouseleave': function(){$("#page1").css('background-image', 'url(images/light.jpg)');}
+});
+$(".circle-3").bind({
+'mouseenter': function(){$("#page1").css('background-image', 'url(images/back3.jpg)');}, 
+'mouseleave': function(){$("#page1").css('background-image', 'url(images/light.jpg)');}
+});
+$(".circle-4").bind({
+'mouseenter': function(){$("#page1").css('background-image', 'url(images/back4.jpg)');}, 
+'mouseleave': function(){$("#page1").css('background-image', 'url(images/light.jpg)');}
+});
